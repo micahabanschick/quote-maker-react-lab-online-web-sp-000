5fbcc98dbe5228a414315e9330926400af239ad9
Micah Banschick <micha.ban@gmail.com>
2021-03-23 19:29:10 +0000

    Automatically backed up by Learn

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -46,7 +46,9 @@ class QuoteForm extends Component {
                     <div className="col-md-5">
                       <textarea
                         className="form-control"
+                        name="content"
                         value={this.state.content}
+                        onChange={this.handleOnChange}
                       />
                     </div>
                   </div>
